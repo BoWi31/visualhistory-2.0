a9c7445ebd001dfa9f8436eef4499b62fd2d9af2
BoWi31 <woitun@bowi-peine.eu>
2026-02-24 17:12:12 +0100

    Update analysisContent.ts

--- analysisContent.ts
+++ analysisContent.ts
@@ -1,9 +1,14 @@
+
 import { AnalysisStep } from './bildanalyse/freiheitfuehrtvolk';
+import * as FreiheitData from './bildanalyse/freiheitfuehrtvolk';
+import * as NapoleonData from './bildanalyse/napoleon';
 import { NAPALM_STEPS, AMPEL_FEEDBACK as NAPALM_FEEDBACK, SENSITIVITY as NAPALM_SENSITIVITY } from './bildanalyse/napalm1972';
-import { CHE_STEPS, AMPEL_FEEDBACK as CHE_FEEDBACK, SENSITIVITY as CHE_SENSITIVITY } from './bildanalyse/che';
+import { CHE_STEPS, AMPEL_FEEDBACK as CHE_FEEDBACK } from './bildanalyse/che';
+import { BIGFOOT_STEPS, AMPEL_FEEDBACK as BIGFOOT_FEEDBACK } from './bildanalyse/pattersonGimlin';
 
 /**
  * Registry für Bildanalysen.
+ * Hier werden alle Bilder registriert, damit sie das Ampel-Feedback korrekt laden.
  */
 export const CONTENT_REGISTRY: Record<string, { 
   steps: AnalysisStep[], 
@@ -17,14 +22,25 @@ export const CONTENT_REGISTRY: Record<string, {
     }
   }
 }> = {
+  "napoleon-1801": {
+    steps: NapoleonData.NAPOLEON_STEPS,
+    ampelFeedback: NapoleonData.AMPEL_FEEDBACK
+  },
+  "che-portraet-1960": {
+    steps: CHE_STEPS,
+    ampelFeedback: CHE_FEEDBACK
+  },
+  "patterson-gimlin-1967": {
+    steps: BIGFOOT_STEPS,
+    ampelFeedback: BIGFOOT_FEEDBACK
+  },
   "napalm-1972": {
     steps: NAPALM_STEPS,
     ampelFeedback: NAPALM_FEEDBACK,
     sensitivity: NAPALM_SENSITIVITY
   },
-  "che-portraet-1960": {
-    steps: CHE_STEPS,
-    ampelFeedback: CHE_FEEDBACK,
-    sensitivity: CHE_SENSITIVITY
+  "freiheit-1830": {
+    steps: FreiheitData.STEPS,
+    ampelFeedback: FreiheitData.AMPEL_FEEDBACK
   }
 };
