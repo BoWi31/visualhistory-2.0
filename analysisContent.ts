2c7f1d8c0803798a1a0b66ca73d466d963370faf
BoWi31 <woitun@bowi-peine.eu>
2026-03-08 11:19:46 +0100

    Update analysisContent.ts

--- analysisContent.ts
+++ analysisContent.ts
@@ -2,6 +2,7 @@ import { AnalysisStep } from './bildanalyse/freiheitfuehrtvolk';
 import { NAPALM_STEPS, AMPEL_FEEDBACK as NAPALM_FEEDBACK, SENSITIVITY as NAPALM_SENSITIVITY } from './bildanalyse/napalm1972';
 import { CHE_STEPS, AMPEL_FEEDBACK as CHE_FEEDBACK, SENSITIVITY as CHE_SENSITIVITY } from './bildanalyse/che';
 import { HITLER_1939_STEPS, AMPEL_FEEDBACK as HITLER_FEEDBACK, SENSITIVITY as HITLER_SENSITIVITY } from './bildanalyse/hitler1939';
+import { EPSTEIN_TRUMP_STEPS, AMPEL_FEEDBACK as EPSTEIN_TRUMP_FEEDBACK } from './bildanalyse/epsteinTrump1997';
 
 /**
  * Registry für Bildanalysen.
@@ -32,5 +33,9 @@ export const CONTENT_REGISTRY: Record<string, {
     steps: HITLER_1939_STEPS,
     ampelFeedback: HITLER_FEEDBACK,
     sensitivity: HITLER_SENSITIVITY
+  },
+  "epstein-trump-1997": {
+    steps: EPSTEIN_TRUMP_STEPS,
+    ampelFeedback: EPSTEIN_TRUMP_FEEDBACK
   }
 };
